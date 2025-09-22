d07cd90d7c5109b07a2a7e8036b799bf094e148b
AYOMIFROSH <rhymeanabel@gmail.com>
2025-09-22 13:15:08 +0100

    vercel analytics

--- cereforge/src/App.tsx
+++ cereforge/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import LoginPage from './components/LoginPage';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <Router>
+      < Analytics />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
