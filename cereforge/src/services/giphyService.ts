171e4461f236fd6ad986f0d626773620a913458d
AYOMIFROSH <rhymeanabel@gmail.com>
2025-10-20 05:25:12 +0100

    gigphy updates

--- cereforge/src/services/giphyService.ts
+++ cereforge/src/services/giphyService.ts
@@ -1,6 +1,6 @@
 // src/services/giphyService.ts
 
-const GIPHY_API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+const GIPHY_API_KEY = import.meta.env.GIPHY_API_KEY;
 const GIPHY_BASE_URL = 'https://api.giphy.com/v1';
 
 export interface GiphyImage {
